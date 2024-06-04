a6fdb13e91a0bfec88a52c74d5ac2541ec8feb15
iheb <117437699+ihebKedri@users.noreply.github.com>
2024-06-04 08:40:46 +0100

    firstNonRepeatedCharacter.js
     firstNonRepeatedCharacter

---
+++ firstNonRepeatedCharacter.js
@@ -0,0 +1,14 @@
+/**
+ *  Given an arbitrary input string, return the first nonrepeated character in
+ *  the string. For example:
+ *  firstNonRepeatedCharacter('ABA'); // => 'B'
+ *  firstNonRepeatedCharacter('AACBDB'); // => 'C'
+ *  Should return null or empty string of there is no repeated characters
+ * 
+ * @function firstNonRepeatedCharacter
+ * @param {String} string 
+ * @returns {String}
+ */
+const firstNonRepeatedCharacter = function (string) {
+  // TODO: your solution here
+}
